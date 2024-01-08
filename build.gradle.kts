297a092d3de74a352ce9df8d70292c8084e9c7b1
Thibaut Gilbert <47tibo@gmail.com>
2024-01-08 17:34:44 +0100

    build: add ktlint & kover for linting and code coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+    id("org.jmailen.kotlinter") version "4.1.1" apply false
+    id("org.jetbrains.kotlinx.kover") version "0.7.5"
 }
