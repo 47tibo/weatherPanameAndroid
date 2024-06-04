7f6ac1895cf4f7560b65efb2ac799f2917c3aa82
Thibaut Gilbert <47tibo@gmail.com>
2024-06-04 12:27:10 +0200

    refactor(weekly-weather-ui): move viewmodel injection to statefull route composable

--- weekly-weather-ui/src/main/java/com/tibo47/weatherPaname/weeklyWeatherUi/navigation/WeeklyWeatherNavigation.kt
+++ weekly-weather-ui/src/main/java/com/tibo47/weatherPaname/weeklyWeatherUi/navigation/WeeklyWeatherNavigation.kt
@@ -1,25 +1,13 @@
 package com.tibo47.weatherPaname.weeklyWeatherUi.navigation
 
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.tibo47.weatherPaname.weeklyWeatherUi.ui.WeeklyWeatherScreen
-import com.tibo47.weatherPaname.weeklyWeatherUi.ui.WeeklyWeatherUiState
-import com.tibo47.weatherPaname.weeklyWeatherUi.ui.WeeklyWeatherViewModel
+import com.tibo47.weatherPaname.weeklyWeatherUi.ui.WeeklyWeatherRoute
 
 public const val WEEKLY_WEATHER_ROUTE: String = "weekly-weather"
 
 public fun NavGraphBuilder.weeklyWeatherScreen() {
     composable(WEEKLY_WEATHER_ROUTE) {
-        val viewModel: WeeklyWeatherViewModel = hiltViewModel()
-        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-        when (uiState) {
-            WeeklyWeatherUiState.Failure -> Text(text = "Failure somewhere")
-            WeeklyWeatherUiState.Loading -> Text(text = "Loading")
-            is WeeklyWeatherUiState.Success -> WeeklyWeatherScreen(uiState = uiState as WeeklyWeatherUiState.Success)
-        }
+        WeeklyWeatherRoute()
     }
 }
--- weekly-weather-ui/src/main/java/com/tibo47/weatherPaname/weeklyWeatherUi/ui/WeeklyWeatherScreen.kt
+++ weekly-weather-ui/src/main/java/com/tibo47/weatherPaname/weeklyWeatherUi/ui/WeeklyWeatherScreen.kt
@@ -1,7 +1,25 @@
 package com.tibo47.weatherPaname.weeklyWeatherUi.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@Composable
+internal fun WeeklyWeatherRoute(
+    modifier: Modifier = Modifier,
+    viewModel: WeeklyWeatherViewModel = hiltViewModel(),
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    when (uiState) {
+        WeeklyWeatherUiState.Failure -> Text(text = "Failure somewhere")
+        WeeklyWeatherUiState.Loading -> Text(text = "Loading")
+        is WeeklyWeatherUiState.Success -> WeeklyWeatherScreen(uiState = uiState as WeeklyWeatherUiState.Success)
+    }
+}
 
 @Composable
 internal fun WeeklyWeatherScreen(uiState: WeeklyWeatherUiState.Success) {
